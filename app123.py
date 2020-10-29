b8e16bfed5f215b89e013828391ef4d85f34215a
jeonghoon96 <dlue1208@gmail.com>
2020-10-29 17:30:37 +0900

    Thread complete

--- app123.py
+++ app123.py
@@ -12,6 +12,7 @@ push_service = FCMNotification(api_key="AAAA6oFTOZk:APA91bGV8mG3Qm5sVNHc93ek8vei
 mToken = ""
 n = 0
 id = ""
+th = 0
 
 cred = credentials.Certificate('./FCM_key.json')
 firebase_admin.initialize_app(cred,{
@@ -19,8 +20,10 @@ firebase_admin.initialize_app(cred,{
 })
 
 def th_read():
-    print("thread starts!")
-    read_data() 
+    global th
+    th = th + 1
+    print(str(th)+"thread!") 
+    print(time.time())
     threading.Timer(5, th_read).start() #5초마다 함수실행
 
 def camera():
@@ -36,19 +39,18 @@ def camera():
     return render_template('index.html', temper = 0, temperature = ret_tem_n, humidiy = ret_hum_n, gas = ret_gas_n, somebody = ret_some)
     
 
-def sendMessage():
-	global n
-	n += 1
-	registration_id = mToken
+def sendMessage(mToken):
+    global n
+    n += 1
+    registration_id = mToken
+    data_message = {
+	    "body" : str(n) + "test"
+    }
 
-	data_message = {
-		"body" : str(n) + "test"
-	}
-
-	message_title = "Push test"
-	message_body = "Hi chiho"
-	result = push_service.notify_single_device(registration_id = registration_id, message_title = message_title, message_body = message_body)
-	print(result)
+    message_title = "Push test"
+    message_body = "Hi chiho"
+    result = push_service.notify_single_device(registration_id = registration_id, message_title = message_title, message_body = message_body)
+    print(result)
 
 
 
@@ -58,29 +60,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-	return render_template('index1.html')
+    print(id)
+    return render_template('index1.html', thread = th)
 
 @app.route('/push')
 def push():
-	ref = 'users/'	
-	new_ref = ref+id
-	dir = db.reference(new_ref)
-	
-	temp = dir.get()
-	for key in temp.keys():
-		if(key == 'token'):
-			mToken = temp[key]
-			print(mToken)
+    ref = 'users/'	
+    new_ref = ref+id
+    dir = db.reference(new_ref)	
+    temp = dir.get()
+    for key in temp.keys():
+        if(key == 'token'):
+            mToken = temp[key]
+            print(mToken)
 
-	return render_template('index1.html')
+    sendMessage(mToken)
+    return render_template('index1.html')
 
 @app.route('/id/<_id>')
 def hello(_id):
-	global id
-	id = _id
-	print(id)
-	return render_template('index.html',user_id = id)
+    global id
+    id = _id
+    print(id)
+    return render_template('index.html',user_id = id)
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+    th_read()
+    app.run(debug=False, host='0.0.0.0')
